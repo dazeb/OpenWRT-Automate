260e74589012534033b3cbf2131f692e7fe6144f
Darren Bennett <dbennett2911@gmail.com>
2024-01-15 16:14:55 +0000

    Create change-swap-size.sh

---
+++ change-swap-size.sh
@@ -0,0 +1,20 @@
+# Find swap location
+DIR="$(uci -q get fstab.extroot.target)"
+
+# Disable and remove the existing swap file
+swapoff "${DIR}/swap"
+rm -f "${DIR}/swap"
+
+# Create a new swap file of 1GB
+dd if=/dev/zero of="${DIR}/swap" bs=1M count=1024
+mkswap "${DIR}/swap"
+
+# Enable swap file
+uci -q delete fstab.swap
+uci set fstab.swap="swap"
+uci set fstab.swap.device="${DIR}/swap"
+uci commit fstab
+service fstab boot
+
+# Verify swap status
+cat /proc/swaps
